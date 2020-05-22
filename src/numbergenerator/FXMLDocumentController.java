44e28b62074f9da1d849517af60e2d937360e710
Nitesh yadav <niteshyadav585@gmail.com>
2020-05-22 14:12:34 +0530

    Add files via upload

---
+++ src/numbergenerator/FXMLDocumentController.java
@@ -0,0 +1,131 @@
+package numbergenerator;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author nitesh
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    
+    private Generator generate;
+    
+    @FXML
+    private Label label, numbertext, result;
+
+    @FXML
+    private Button btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9,btn0,enter,back, backButton;
+
+    private String tmp="";
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) throws InterruptedException, IOException {
+        int len = numbertext.getText().length();
+        if(len>7){
+            label.setText("Number should be 7 digit.");
+        }else if(len==7){
+            List<String> results = generate.buildString(Integer.parseInt(numbertext.getText()));
+            if(!results.isEmpty()){
+              for (int i=0; i<results.size();) {
+                    for (int j = 0; j<4 &&  i<results.size() ; j++) {
+                        tmp += (results.get(i) + ", ");
+                        i++;
+                    }
+                    tmp+=("\n");
+                }
+                
+                result.setText(tmp);
+               
+            }else{
+                label.setText("Number should not contain 1 or 0.");
+            }
+        }else{
+            label.setText("Number should be 7 digit.");
+        }
+        
+    }
+    
+    
+    @FXML
+    private void handleBtn1(ActionEvent event) throws InterruptedException{
+        label.setText("");
+        numbertext.setText(numbertext.getText()+"1");
+    }
+    
+    @FXML
+    private void handleBtn2(ActionEvent event){
+        label.setText("");
+        numbertext.setText(numbertext.getText()+"2");
+    }
+    
+    @FXML
+    private void handleBtn3(ActionEvent event){
+        label.setText("");
+        numbertext.setText(numbertext.getText()+"3");
+    }
+    
+    @FXML
+    private void handleBtn4(ActionEvent event){
+        label.setText("");
+        numbertext.setText(numbertext.getText()+"4");
+    }
+    
+    @FXML
+    private void handleBtn5(ActionEvent event){
+        label.setText("");
+        numbertext.setText(numbertext.getText()+"5");
+    }
+    
+    @FXML
+    private void handleBtn6(ActionEvent event){
+        label.setText("");
+        numbertext.setText(numbertext.getText()+"6");
+    }
+    
+    @FXML
+    private void handleBtn7(ActionEvent event){
+        label.setText("");
+        numbertext.setText(numbertext.getText()+"7");
+    }
+    
+    @FXML
+    private void handleBtn8(ActionEvent event){
+        label.setText("");
+        numbertext.setText(numbertext.getText()+"8");
+    }
+    
+    @FXML
+    private void handleBtn9(ActionEvent event){
+        label.setText("");
+        numbertext.setText(numbertext.getText()+"9");
+    }
+    
+    @FXML
+    private void handleBtn0(ActionEvent event){
+        label.setText("");
+        numbertext.setText(numbertext.getText()+"0");
+    }
+    
+    
+    @FXML
+    private void backspace(ActionEvent event){
+        label.setText("");
+        String s =  numbertext.getText();
+        numbertext.setText(s.substring(0,s.length()-1));
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+       generate = new Generator();
+    }    
+    
+}
---
+++ src/numbergenerator/NumberGenerator.java
@@ -0,0 +1,33 @@
+package numbergenerator;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Nitesh
+ */
+public class NumberGenerator extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
